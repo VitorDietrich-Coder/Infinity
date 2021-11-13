940dfb2fc79db946ee84cec0d1341b120da5051b
VitorDietrich-coder <vitorgames078@gmail.com>
2021-11-13 20:44:53 -0300

    commit

--- Localização.php
+++ Localização.php
@@ -19,7 +19,7 @@
     <h1 class="endereço">"R. Honório Lemos, 314 - Jardim Panorama, Toledo - PR, 85911-060"</h1>
         <div id="maps" class="map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d905.7701121945128!2d-53.73208267079271!3d-24.75843068117376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f3bfe6ca9f4c83%3A0xfec24a3a8368ad23!2sR.%20Hon%C3%B3rio%20Lemos%2C%20314%20-%20Jardim%20Panorama%2C%20Toledo%20-%20PR%2C%2085911-060!5e0!3m2!1spt-BR!2sbr!4v1636841796352!5m2!1spt-BR!2sbr" width="800" height="700" style="border:0;" allowfullscreen="" loading="lazy"></iframe> 
-            <a class="link-google"href="https://www.google.com/maps/place/R.+Hon%C3%B3rio+Lemos,+314+-+Jardim+Panorama,+Toledo+-+PR,+85911-060/@-24.758432,-53.731535,19z/data=!4m5!3m4!1s0x94f3bfe6ca9f4c83:0xfec24a3a8368ad23!8m2!3d-24.7584319!4d-53.7315355?hl=pt-BR">LINK DO GOGLE</a>
+            <a class="link-google"href="https://www.google.com/maps/place/R.+Hon%C3%B3rio+Lemos,+314+-+Jardim+Panorama,+Toledo+-+PR,+85911-060/@-24.758432,-53.731535,19z/data=!4m5!3m4!1s0x94f3bfe6ca9f4c83:0xfec24a3a8368ad23!8m2!3d-24.7584319!4d-53.7315355?hl=pt-BR">LINK DO GOOGLE MAPS</a>
         </div>
     </div>
      <?php
